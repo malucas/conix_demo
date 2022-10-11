ae8ffe1d6d1cee3bd476c7621fcb9100e2e2785c
malucas <marcus.a.lucas@gmail.com>
2022-10-11 05:47:48 -0700

    fixed bug with cmd_interface subscriber initilization

--- src/cmd_interface.cpp
+++ src/cmd_interface.cpp
@@ -5,21 +5,20 @@
 #include <cmath>
 
 #include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
 #include "ros2_unitree_legged_msgs/msg/high_cmd.hpp"
 #include "ros2_unitree_legged_msgs/msg/high_state.hpp"
-#include "ros2_unitree_legged_msgs/msg/low_cmd.hpp"
-#include "ros2_unitree_legged_msgs/msg/low_state.hpp"
-#include "unitree_legged_sdk/unitree_legged_sdk.h"
-#include "convert.h"
+#include "unitree_legged_sdk/comm.h"
 
 using namespace UNITREE_LEGGED_SDK;
 using namespace std::chrono_literals;
+using std::placeholders::_1;
 
 class CmdInterface : public rclcpp::Node
 {
     public:
         CmdInterface()
-        : Node("command_interface"), count_(0)
+        : Node("command_interface"), count_ {0}
         {
             std::cout << "WARNING: Control level is set to HIGH-level." << std::endl
               << "Make sure the robot is standing on the ground." << std::endl
@@ -27,35 +26,44 @@ class CmdInterface : public rclcpp::Node
             std::cin.ignore();
 
             timer_ = this->create_wall_timer(200ms, std::bind(&CmdInterface::timer_callback, this));
-            publisher_ = this->create_publisher<ros2_unitree_legged_msgs::msg::HighCmd>("high_cmd", 1);
+            high_cmd_pub_ = this->create_publisher<ros2_unitree_legged_msgs::msg::HighCmd>("high_cmd", 1);
+            cmd_vel_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
+                "cmd_vel_sub", 1, std::bind(&CmdInterface::cmd_vel_callback, this, _1));
         }
     
     private:
         void timer_callback()
         {
-            ros2_unitree_legged_msgs::msg::HighCmd high_cmd_ros;
-            
-            high_cmd_ros.head[0] = 0xFE;
-            high_cmd_ros.head[1] = 0xEF;
-            high_cmd_ros.level_flag = HIGHLEVEL;
-            high_cmd_ros.mode = 0;
-            high_cmd_ros.gait_type = 0;
-            high_cmd_ros.speed_level = 0;
-            high_cmd_ros.foot_raise_height = 0;
-            high_cmd_ros.body_height = 0;
-            high_cmd_ros.euler[0] = 0;
-            high_cmd_ros.euler[1] = 0;
-            high_cmd_ros.euler[2] = 0;
-            high_cmd_ros.velocity[0] = 0.0f;
-            high_cmd_ros.velocity[1] = 0.0f;
-            high_cmd_ros.yaw_speed = 0.0f;
-            high_cmd_ros.reserve = 0;
+            high_cmd_ros_.head[0] = 0xFE;
+            high_cmd_ros_.head[1] = 0xEF;
+            high_cmd_ros_.level_flag = HIGHLEVEL;
+            high_cmd_ros_.mode = 0;
+            high_cmd_ros_.gait_type = 0;
+            high_cmd_ros_.speed_level = 0;
+            high_cmd_ros_.foot_raise_height = 0;
+            high_cmd_ros_.body_height = 0;
+            high_cmd_ros_.euler[0] = 0;
+            high_cmd_ros_.euler[1] = 0;
+            high_cmd_ros_.euler[2] = 0;
+            high_cmd_ros_.velocity[0] = 0.0f;
+            high_cmd_ros_.velocity[1] = 0.0f;
+            high_cmd_ros_.yaw_speed = 0.0f;
+            high_cmd_ros_.reserve = 0;
 
-            publisher_->publish(high_cmd_ros);
+            high_cmd_pub_->publish(high_cmd_ros_);
+        }
+
+        void cmd_vel_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
+        {
+            return;
         }
 
         rclcpp::TimerBase::SharedPtr timer_;
-        rclcpp::Publisher<ros2_unitree_legged_msgs::msg::HighCmd>::SharedPtr publisher_;
+        rclcpp::Publisher<ros2_unitree_legged_msgs::msg::HighCmd>::SharedPtr high_cmd_pub_;
+        rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr cmd_vel_sub_;
+        rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr cmd_pos_sub_;
+
+        ros2_unitree_legged_msgs::msg::HighCmd high_cmd_ros_;
         uint count_;
 };
 
